e981de0a00abd472016ccb2342648706413837ae
moops <work@raceweb.ca>
2010-09-28 16:54:18 -0700

    first successful functional test (workouts)

--- test/functional/workouts_controller_test.rb
+++ test/functional/workouts_controller_test.rb
@@ -2,44 +2,42 @@ require 'test_helper'
 
 class WorkoutsControllerTest < ActionController::TestCase
   test "should get index" do
-    get :index
+    get :index, {}, {'user_id' => people(:adam).to_param}
     assert_response :success
     assert_not_nil assigns(:workouts)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
   test "should create workout" do
     assert_difference('Workout.count') do
-      post :create, :workout => { :person => people(:adam), :location => 'elk lake', :duration => 25, :workout_on => Date.today }
+      post :create, {:workout => { :person => people(:adam), :location => 'elk lake', :duration => 25, :workout_on => Date.today }}, {'user_id' => people(:adam).to_param}
     end
 
     assert_redirected_to workouts_path
   end
 
   test "should show workout" do
-    get :show, :id => workouts(:one).to_param
+    get :show, {:id => workouts(:one).to_param}, {'user_id' => people(:adam).to_param}
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :id => workouts(:one).to_param
+    get :edit, {:id => workouts(:one).to_param}, {'user_id' => people(:adam).to_param}
     assert_response :success
   end
 
   test "should update workout" do
-    put :update, :id => workouts(:one).to_param, :workout => { }
-    assert_redirected_to workout_path(assigns(:workout))
+    put :update, {:id => workouts(:one).to_param, :workout => { }}, {'user_id' => people(:adam).to_param}
+    assert_redirected_to workouts_path
   end
 
   test "should destroy workout" do
     assert_difference('Workout.count', -1) do
-      delete :destroy, :id => workouts(:one).to_param
+      delete :destroy, {:id => workouts(:one).to_param}, {'user_id' => people(:adam).to_param}
     end
 
     assert_redirected_to workouts_path
   end
 end
+
+
+
