f59873c012fbae39c08f279d3601d68c02e3c2cc
moops <work@raceweb.ca>
2010-10-04 16:40:11 -0700

    fix functional tests for connections and contacts. increase css border-radius to 6.

--- test/functional/connections_controller_test.rb
+++ test/functional/connections_controller_test.rb
@@ -1,43 +1,39 @@
 require 'test_helper'
 
 class ConnectionsControllerTest < ActionController::TestCase
+
   test "should get index" do
-    get :index
+    get :index, {}, {'user_id' => people(:adam).to_param}
     assert_response :success
     assert_not_nil assigns(:connections)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
   test "should create connection" do
     assert_difference('Connection.count') do
-      post :create, :connection => { }
+      post :create, {:connection => { :person => people(:adam), :username => 'moops', :password => 'foobar', :url => 'http://www.nba.com' }}, {'user_id' => people(:adam).to_param}
     end
 
-    assert_redirected_to connection_path(assigns(:connection))
+    assert_redirected_to connections_path
   end
 
   test "should show connection" do
-    get :show, :id => connections(:one).to_param
+    get :show, {:id => connections(:one).to_param}, {'user_id' => people(:adam).to_param}
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :id => connections(:one).to_param
+    get :edit, {:id => connections(:one).to_param}, {'user_id' => people(:adam).to_param}
     assert_response :success
   end
 
   test "should update connection" do
-    put :update, :id => connections(:one).to_param, :connection => { }
-    assert_redirected_to connection_path(assigns(:connection))
+    put :update, {:id => connections(:one).to_param, :connection => { }}, {'user_id' => people(:adam).to_param}
+    assert_redirected_to connections_path
   end
 
   test "should destroy connection" do
     assert_difference('Connection.count', -1) do
-      delete :destroy, :id => connections(:one).to_param
+      delete :destroy, {:id => connections(:one).to_param}, {'user_id' => people(:adam).to_param}
     end
 
     assert_redirected_to connections_path
--- test/functional/contacts_controller_test.rb
+++ test/functional/contacts_controller_test.rb
@@ -1,43 +1,39 @@
 require 'test_helper'
 
 class ContactsControllerTest < ActionController::TestCase
+  
   test "should get index" do
-    get :index
+    get :index, {}, {'user_id' => people(:adam).to_param}
     assert_response :success
     assert_not_nil assigns(:contacts)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
   test "should create contact" do
     assert_difference('Contact.count') do
-      post :create, :contact => { }
+      post :create, {:contact => { :person => people(:adam), :name => 'scooby do' }}, {'user_id' => people(:adam).to_param}
     end
 
-    assert_redirected_to contact_path(assigns(:contact))
+    assert_redirected_to contacts_path
   end
 
   test "should show contact" do
-    get :show, :id => contacts(:one).to_param
+    get :show, {:id => contacts(:one).to_param}, {'user_id' => people(:adam).to_param}
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :id => contacts(:one).to_param
+    get :edit, {:id => contacts(:one).to_param}, {'user_id' => people(:adam).to_param}
     assert_response :success
   end
 
   test "should update contact" do
-    put :update, :id => contacts(:one).to_param, :contact => { }
-    assert_redirected_to contact_path(assigns(:contact))
+    put :update, {:id => contacts(:one).to_param, :contact => { }}, {'user_id' => people(:adam).to_param}
+    assert_redirected_to contacts_path
   end
 
   test "should destroy contact" do
     assert_difference('Contact.count', -1) do
-      delete :destroy, :id => contacts(:one).to_param
+      delete :destroy, {:id => contacts(:one).to_param}, {'user_id' => people(:adam).to_param}
     end
 
     assert_redirected_to contacts_path
