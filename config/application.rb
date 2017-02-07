948f0e0d7f02b1a71f616939029ccc986f656feb
Adam Lawrence <adam@raceweb.ca>
2017-02-07 00:06:23 -0800

    production loading of lib files

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ Bundler.require(*Rails.groups)
 module Brainpad
   class Application < Rails::Application
 
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W{#{config.root}/lib}
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
